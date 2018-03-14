14c5bf0f3632e14539b095b42ef451489601e51d
Rishabh <32431921+rishabhrishabh@users.noreply.github.com>
2018-03-14 23:54:44 +0530

    Revert "Revert "Merge branch 'master' of https://github.com/Manan-YMCA/ElementsCulmyca2018Website""
    This reverts commit 8197f18f2019dd95df5cd03e2b042d53693ceed5.

--- js/pre.js
+++ js/pre.js
@@ -1,14 +1,37 @@
 var images = ["images/home.jpg","images/about/ac.jpg","images/about/wallpaper.jpg","images/about/ay.jpg","images/events/ananya.jpg","images/events/ehome.jpg","images/events/eklavya.jpg","images/events/IEEE.jpg","images/events/jhalak.jpg","images/events/manan.jpg","images/events/mechnext.jpg","images/events/microbird.jpg","images/events/natraja.jpg","images/events/SAE.jpg","images/events/samarpan.jpg","images/events/srijan.jpg","images/events/tarunnun.jpg","images/events/vivekanand.jpg","images/events/vividha.jpg","images/register/wallpaper.jpg","images/back.jpg","images/back.png","images/back2.jpg","images/bg.jpg","images/bg2.jpg"];
 var x1 = 0;
+var percent = 0;
+var i = 0;
 $.preload(images);
 $.preload(images,{
-
   eachOnLoad: function(){
     x1++;
-   if (x1 == images.length)
-   {
-   	$("#preloader").fadeOut('fast');
-   }
+    percent = (x1/(images.length))*100;
+   	$("#percent").text(percent.toFixed(0)+'%');
+   	switch (x1){
+   		case 1:
+   		 $("#uptrap").css('opacity', '1');break;
+   		case 4:
+   		 $("#eside").css('opacity', '1');break;
+   		case 8:
+   		 $("#reside").css('opacity', '1');break;
+   		case 11:
+   		 $("#ebot").css('opacity', '1');break;
+   		case 14:
+   		 $("#upc").css('opacity', '1');break;
+   		case 18:
+   		 $("#lupc").css('opacity', '1');break;
+   		case 21:
+   		 $("#brc").css('opacity', '1');break;
+   		case 25:
+   		 $("#blrc").css('opacity', '1');break;
+   	}
+    if (x1 == images.length)
+    {
+     setTimeout(function(){
+     	$("#preloader").fadeOut('slow');
+     },1000)
+    }
   }
 
 });
